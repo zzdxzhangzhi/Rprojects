aad014b77a55d0e6c33e44b2f46b70868f443357
Zhi Zhang <zzha822@UoA.auckland.ac.nz>
2017-08-02 17:08:48 +1200

    complete lab1 contents

--- 784/predictors.R
+++ 784/predictors.R
@@ -3,16 +3,31 @@ library(R330)
 library(ISLR)
 
 # make linear regression base
-sp500 = lm(Today ~ . - Direction, data = Smarket)
+head(Smarket)
+use = sample(1250, 1000)
+sp500.df = Smarket[use,]
+rownames(sp500.df) = 1:1000
 
-# do simple prediction and caculate the error
-predictions = predict(sp500, data = Smarket)
+sp500 = lm(Today ~ . - Direction, data = sp500.df)
+
+# do simple prediction and caculate the test error
+newSp500.df = Smarket[-use,]
+rownames(newSp500.df) = 1:250
+predictions = predict(sp500, newdata = newSp500.df)
 head(predictions)
-actuals = Smarket$Today
+actuals = newSp500.df$Today
 mean((predictions - actuals)^2)
 
 # Use cross.val from R330 package to do predict error estimation
 cross.val(sp500, nfold = 10, nrep = 20)
+cross.val(sp500, nfold = 10, nrep = 50)
+cross.val(sp500, nfold = 10, nrep = 100)
+cross.val(sp500, nfold = 5, nrep = 20)
+cross.val(sp500, nfold = 10, nrep = 50)
+cross.val(sp500, nfold = 10, nrep = 100)
+
+# apparent error
+mean(residuals(sp500)^2)
 
 ####################################################################
 cross.val.mod <- function (f, nfold = 10, nrep = 20, ...){
@@ -52,11 +67,74 @@ for (i in 1:20) {
 mean(cvvec)
 sd(cvvec)
 
-# Load libraries
+# Use bootstrap method to estimate PE
+err.boot(sp500, B = 50)
+
+# Use boostrap package
 library(bootstrap)
+theta.fit = function(x, y) {
+			lsfit(x, y)
+		}
+theta.predict = function(fit, x) {
+			cbind(1, x)%*%fit$coef
+			}
+sq.err = function(y, yhat) {
+		(y - yhat)^2
+		}
+
+# CV10 estimation
+y = sp500.df[,1]
+x = sp500.df[,-1]
+cv10Errs = crossval(x, y, theta.fit, theta.predict, ngroup = 10)
+cv10 = mean((y - cv10Errs$cv.fit)^2)
+cv10
+
+boot = bootpred(x, y, nboot = 200, theta.fit, theta.predict,
+			err.meas = sq.err)
+
+# Use caret package
 library(caret)
 
-# Use bootstrap method to estimate PE
+# CV10 estimation with caret
+CV10 = train(Today ~ + Year + Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume - Direction, 
+		data = sp500.df, 
+		method = "lm",
+		trControl = trainControl(method = "cv", number = 10, repeats = 20))
+CV10
+
+# bootstrap estimate
+boot = train(Today~ . - Direction, 
+		data = sp500.df,
+		method = "lm", 
+		trControl = trainControl(method = "boot", repeats = 200))
+boot
+
+# forward selection
+null.model = lm(Today~1, data = sp500.df)
+selected = step(null.model, 
+		scope = formula(sp500), 
+		direction = "forward",
+		trace = 0)
+selected
+
+# backward elimination
+selected = step(sp500, 
+		scope = formula(sp500),
+		direction = "backward", 
+		trace = 0)
+selected
+
+# stepwise
+selected = step(sp500, 
+		scope = formula(sp500),
+		direction = "both", trace = 0)
+selected
+
+# all possible regressions
+allpossregs(sp500)
+
+
+
 
 
 
